4e998ae31f00c7fa251e41897638de0b4b2c768d
theselegadev <matheus.seleghin7@gmail.com>
2024-12-05 20:45:50 -0300

    [feature] implementação de inputs + validação + modularização

---
+++ modules.js
@@ -0,0 +1,8 @@
+
+export function verify (inp1,inp2){
+    if(inp1.value != '' && inp2.value != ''){
+        return true
+    }else{
+        return false
+    }
+}
--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+import { verify } from "./modules.js"
+
 const card_rules = document.getElementById('card-rules')
 const btn_close = document.getElementById('btn-close')
 const btn_rules = document.getElementById('btn-rules')
+const input_player01 = document.getElementById('player01')
+const input_player02 = document.getElementById('player02')
+const btn_play = document.getElementById('btn-play')
+const button_play = document.getElementById('button-play')
+
+console.log(btn_play)
 
 btn_rules.addEventListener('click',()=>{
     card_rules.style.top = "30%"
@@ -13,3 +21,11 @@ btn_close.addEventListener('click',()=>{
 setTimeout(()=>{
     card_rules.style.top = "30%"
 },1300)
+
+const myinterval = setInterval(()=>{
+    console.log('setInterval rodando')
+    if(verify(input_player01,input_player02)){
+        btn_play.style.display = "block"
+        clearInterval(myinterval)
+    }
+},2000)
