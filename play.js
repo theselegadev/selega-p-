4659c5a7de7b90920db0656a944c0a411e8ed03e
theselegadev <matheus.seleghin7@gmail.com>
2024-12-08 21:26:17 -0300

    [feature] Inserindo imagens das escolhas dos jogadores

--- play.js
+++ play.js
@@ -3,6 +3,8 @@ class selega_po{
         manipulation_choice.show_card()
         manipulation_choice.alter_names()
         manipulation_names.insert_names()
+        manipulation_images.showImages("choice_player1")
+        manipulation_images.showImages("choice_player2")
     }
 }
 
@@ -10,6 +12,7 @@ class manipulation_choice{
    static card_choices = document.getElementById('card-choices')
    static card_name_player = document.getElementById('card-name-player')
    static background = document.getElementById('background')
+   static closeCard = false
    
    static insertChoice(value){
         if(manipulation_choice.card_name_player.innerText == manipulation_names.name_player01){
@@ -18,6 +21,7 @@ class manipulation_choice{
             localStorage.setItem("choice_player2",value)
             manipulation_choice.card_choices.style.display = "none"
             this.background.style.filter = "blur(0px)"
+            this.closeCard = true
         }
    }
 
@@ -55,6 +59,29 @@ class manipulation_names{
 class manipulation_images{
     static image_choice_player1 = document.getElementById('image-choice-player1')
     static image_choice_player2 = document.getElementById('image-choice-player2')
+    
+    static showImages(player){
+        const myinterval = setInterval(()=>{
+            if(manipulation_choice.closeCard == true){
+                if(localStorage.getItem(player) == 'pedra' && player == "choice_player1"){
+                    this.image_choice_player1.src = "./images/pedra.png"
+                }else if(localStorage.getItem(player) == 'papel' && player == "choice_player1"){
+                    this.image_choice_player1.src = "./images/papel.png"
+                }else if(localStorage.getItem(player) == 'tesoura' && player == "choice_player1"){
+                    this.image_choice_player1.src = "./images/tesoura.png"
+                }else if(localStorage.getItem(player) == 'pedra'){
+                    this.image_choice_player2.src = "./images/pedra.png"
+                    clearInterval(myinterval)
+                }else if(localStorage.getItem(player) == 'papel'){
+                    this.image_choice_player2.src = "./images/papel.png"
+                    clearInterval(myinterval)
+                }else if(localStorage.getItem(player) == 'tesoura'){
+                    this.image_choice_player2.src = "./images/tesoura.png"
+                    clearInterval(myinterval)
+                }
+            }
+        },100)
+    }
 }
 
 selega_po.play()
